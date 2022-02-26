7559a5f7eb5b986762a0c248ab988e0b0e0d8221
isiziakov <ivan.siziakov@mail.ru>
2022-02-26 22:25:54 +0300

    View test

--- Lab2/Lab2/Model/Flat.cs
+++ Lab2/Lab2/Model/Flat.cs
@@ -16,6 +16,8 @@ namespace Lab2.Model
         [Required]
         public byte floor { get; set; }
         [Required]
+        public byte room { get; set; }
+        [Required]
         [StringLength(50)]
         public string material { get; set; }
         public virtual District District { get; set; }
@@ -24,12 +26,13 @@ namespace Lab2.Model
         {
 
         }
-        public Flat(int district_id, double square, byte floor, string material)
+        public Flat(int district_id, double square, byte floor, string material, byte room)
         {
             this.district_id = district_id;
             this.square = square;
             this.floor = floor;
             this.material = material;
+            this.room = room;
         }
     }
 }
